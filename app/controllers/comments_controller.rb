f409faae71a8f622af04ba2996c3b688ec484fb1
yukifu027 <63824425+yukifu027@users.noreply.github.com>
2020-05-03 17:37:39 +0900

    いいね機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
     comment = Comment.create(comment_params)
     redirect_to "/guides/#{comment.guide.id}" 
   end
-
+  
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, guide_id: params[:guide_id])
--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,13 +1,15 @@
 class GuidesController < ApplicationController
   def index
     @guides = Guide.all
-    @guide = Guide.find_by(params[:id])
+    @likes = Like.where(user_id: current_user.id)
   end
 
   def show
-    @user = User.find_by(params[:id])
-    @guide = Guide.find_by(params[:id])
+    @user = User.find(params[:id])
+    @guide = Guide.find(params[:id])
     @comment = Comment.new
     @comments = @guide.comments.includes(:user)
+    @likes_count = Like.where(guide_id: @guide.id).count
+    @likes = Like.where(user_id: current_user.id)
   end
 end

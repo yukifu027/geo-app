fedf726a86fd366e7ead3715eda18fbd281e920a
yukifu027 <63824425+yukifu027@users.noreply.github.com>
2020-06-12 18:36:52 +0900

    ガイド内容のCSS修正

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,7 +1,7 @@
 class PicturesController < ApplicationController
 
   def index
-    @pictures = Picture.where(guide_id: 1)
+    @pictures = Picture.where(guide_id: params[:guide_id])
     @likes = Like.where(user_id: current_user.id)
     @solds = UserGuide.where(user_id: current_user.id)
   end
